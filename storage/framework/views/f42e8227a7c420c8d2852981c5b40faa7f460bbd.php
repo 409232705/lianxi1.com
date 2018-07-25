4da52937ee5ae26bbb75a4a2ad0fb98498aadbb8
409232705 <409232705@qq.com>
2018-07-25 23:59:18 +0800

    Finish static pages

---
+++ storage/framework/views/f42e8227a7c420c8d2852981c5b40faa7f460bbd.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $__env->yieldContent('title', 'Sample App'); ?> - Laravel 新手入门教程</title>
+</head>
+<body>
+<?php echo $__env->yieldContent('content'); ?>
+</body>
+</html>
