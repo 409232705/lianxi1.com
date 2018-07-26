e5af56ac62909b75c72c1ed6efcf4ca690b9ef50
409232705 <409232705@qq.com>
2018-07-26 21:34:33 +0800

    Finish layout and routes

--- storage/framework/views/ac1833cdd7d59255325ac0d687903bb2df7a2747.php
+++ storage/framework/views/ac1833cdd7d59255325ac0d687903bb2df7a2747.php
@@ -1,5 +1,17 @@
 
+
 <?php $__env->startSection('content'); ?>
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a> 的项目主页。
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="<?php echo e(route('signup')); ?>" role="button">现在注册</a>
+        </p>
+    </div>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.default', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
